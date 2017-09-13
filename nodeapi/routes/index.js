9f29433db2dfde7f99677c3e73a624dba19f24d7
Jordi Barrero <jordibar@gmail.com>
2017-09-13 21:39:16 +0200

    Probando Express

--- nodeapi/routes/index.js
+++ nodeapi/routes/index.js
@@ -11,4 +11,22 @@ router.get('/hola', (req, res, next) => {
   res.render('hola');
 });
 
+// recibimos parámetros en la ruta
+router.get('/ruta/:algo', (req, res, next) => { //Podemos tener 'ruta7:algo?' sería opcional
+  console.log('parámetro en ruta', req.params);
+  res.send('ok ' + req.params.algo);
+});
+
+// Hacemos otra ruta con más de un parámetro
+router.get('/calle/:calle/numero/:numero([0-9]+)/piso/:piso/puerta/:puerta', (req, res, next) => {
+  console.log('parámetro en ruta', req.params);
+  res.send('ok');
+});
+
+
+router.get('/query', (req, res, next) => {
+  console.log('parámetro en query string', req.query);
+  res.send('ok');
+});
+
 module.exports = router;
